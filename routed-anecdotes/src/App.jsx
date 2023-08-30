3c88600990c4590cf3680b41d07b0736d3663544
AzeHie <109618459+AzeHie@users.noreply.github.com>
2023-08-30 17:34:42 +0300

    Fixed exercise 7.1, Footer moved to the bottom

--- routed-anecdotes/src/App.jsx
+++ routed-anecdotes/src/App.jsx
@@ -6,7 +6,6 @@ import CreateNew from './components/CreateNew';
 import Footer from './components/Footer';
 import Menu from './components/Menu';
 
-
 const App = () => {
   const [anecdotes, setAnecdotes] = useState([
     {
@@ -47,17 +46,16 @@ const App = () => {
 
   return (
     <Fragment>
-      <div>
-        <h1>Software anecdotes</h1>
-        <Menu />
-        <Footer />
-      </div>
+      <h1>Software anecdotes</h1>
+      <Menu />
 
       <Routes>
         <Route path='/' element={<AnecdoteList anecdotes={anecdotes} />} />
         <Route path='/create' element={<CreateNew addNew={addNew} />} />
         <Route path='/about' element={<About />} />
       </Routes>
+      
+      <Footer />
     </Fragment>
   );
 };
